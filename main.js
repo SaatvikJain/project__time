717c09e2c03d916afd97c6eaa73cf69eb329482e
SaatvikJain <84066772+SaatvikJain@users.noreply.github.com>
2021-07-15 17:45:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas= createcanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('SaatvikFilterapp.png');
+}
